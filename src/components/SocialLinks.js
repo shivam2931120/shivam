38df262da9e2fcf52aecff2bbcf48f7ca4af04f1
Shivam <154829012+shivam2931120@users.noreply.github.com>
2025-03-10 12:57:54 +0530

    SocialLinks.js

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -4,20 +4,20 @@ const SocialLinks = () => {
   return (
     <div className="social-container">
       <div className="main-icon">
-        <img src="/social-media.png" alt="Social" />
+        <img src="https://img.icons8.com/?size=100&id=rJh3Z5RsEUSA&format=png&color=000000" alt="Social" />
       </div>
       <div className="social-links">
-        <a href="https://www.linkedin.com/in/shivam-28bbb92ab/" target="_blank" rel="noopener noreferrer">
-          <img src="/linkedin.png" alt="LinkedIn" />
+        <a href="https://www.linkedin.com/in/shivam-28bbb92ab" target="_blank" rel="noopener noreferrer">
+          <img src="https://img.icons8.com/?size=100&id=xuvGCOXi8Wyg&format=png&color=000000" alt="LinkedIn" />
         </a>
         <a href="https://github.com/shivam2931120" target="_blank" rel="noopener noreferrer">
-          <img src="/github.png" alt="GitHub" />
+          <img src="https://img.icons8.com/?size=100&id=3tC9EQumUAuq&format=png&color=000000" alt="GitHub" />
         </a>
         <a href="mailto:shivam.bgp@outlook.com">
-          <img src="/gmail.png" alt="Email" />
+          <img src="https://img.icons8.com/?size=100&id=ut6gQeo5pNqf&format=png&color=000000" alt="Email" />
         </a>
         <a href="https://leetcode.com/u/Shivam2931120/" target="_blank" rel="noopener noreferrer">
-          <img src="/leetcode.png" alt="LeetCode" />
+          <img src="https://img.icons8.com/?size=100&id=wDGo581Ea5Nf&format=png&color=000000" alt="LeetCode" />
         </a>
       </div>
     </div>
